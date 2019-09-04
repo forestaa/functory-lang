0c175c90d61eb0a91290a747d034c9eb0eea54f6
forestaa <t.daichi.3@gmail.com>
2019-09-05 00:13:14 +0900

    add item to edge

--- src/Functory/Graph.hs
+++ src/Functory/Graph.hs
@@ -5,19 +5,20 @@ import RIO
 import qualified RIO.Set as Set
 
 
-newtype Vertex a = Vertex a deriving (Show, Eq, Ord) via a
-newtype Edge a = Edge (Record '["source" >: Vertex a, "target" >: Vertex a]) deriving (Show, Eq, Ord) via (Record '["source" >: Vertex a, "target" >: Vertex a])
-newtype Graph a = Graph (Record '["vertices" >: Set.Set (Vertex a), "edges" >: Set.Set (Edge a)]) deriving (Show, Eq) via (Record '["vertices" >: Set.Set (Vertex a), "edges" >: Set.Set (Edge a)])
 
-newEdge :: Vertex a -> Vertex a -> Edge a
-newEdge src tgt = Edge $ #source @= src <: #target @= tgt <: nil
-newGraph :: Ord a => [Vertex a] -> [Edge a] -> Graph a
+newtype Vertex b = Vertex b deriving (Show, Eq, Ord) via b
+newtype Edge a b = Edge (Record '["item" >: a, "source" >: Vertex b, "target" >: Vertex b]) deriving (Show, Eq, Ord) via (Record '["item" >: a, "source" >: Vertex b, "target" >: Vertex b])
+newtype Graph a b = Graph (Record '["vertices" >: Set.Set (Vertex b), "edges" >: Set.Set (Edge a b)]) deriving (Show, Eq) via (Record '["vertices" >: Set.Set (Vertex b), "edges" >: Set.Set (Edge a b)])
+
+newEdge :: a -> Vertex b -> Vertex b -> Edge a b
+newEdge a src tgt = Edge $ #item @= a <: #source @= src <: #target @= tgt <: nil
+newGraph :: (Ord a, Ord b) => [Vertex b] -> [Edge a b] -> Graph a b
 newGraph vertices edges = Graph $ #vertices @= Set.fromList vertices <: #edges @= Set.fromList edges <: nil
-addVerticesToGraph :: Ord a => [Vertex a] -> Graph a -> Graph a
+addVerticesToGraph :: Ord b => [Vertex b] -> Graph a b -> Graph a b
 addVerticesToGraph vertices (Graph r) = Graph $ over #vertices (flip (foldr Set.insert) vertices) r
-addEdgesToGraph :: Ord a => [Edge a] -> Graph a -> Graph a
+addEdgesToGraph :: (Ord a, Ord b) => [Edge a b] -> Graph a b -> Graph a b
 addEdgesToGraph edges (Graph r) = Graph $ over #edges (flip (foldr Set.insert) edges) r
-addVerticesAndEdgesToGraph :: Ord a => [Vertex a] -> [Edge a] -> Graph a -> Graph a
+addVerticesAndEdgesToGraph :: (Ord a, Ord b) => [Vertex b] -> [Edge a b] -> Graph a b -> Graph a b
 addVerticesAndEdgesToGraph vertices edges = addEdgesToGraph edges . addVerticesToGraph vertices
-union :: Ord a => Graph a -> Graph a -> Graph a
+union :: (Ord a, Ord b) => Graph a b -> Graph a b -> Graph a b
 union (Graph r) (Graph r') = Graph $ #vertices @= ((r ^. #vertices) `Set.union` (r' ^. #vertices)) <: #edges @= ((r ^. #edges) `Set.union` (r' ^. #edges)) <: nil
